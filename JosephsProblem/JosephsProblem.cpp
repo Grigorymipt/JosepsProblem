101bf038496eebc70e423079619c9b6fcc9e9e64
Grigorymipt <91215075+Grigorymipt@users.noreply.github.com>
2022-03-01 09:54:26 +0300

    Update JosephsProblem.cpp

--- JosephsProblem/JosephsProblem.cpp
+++ JosephsProblem/JosephsProblem.cpp
@@ -1,18 +1,8 @@
-// JosephsProblem.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-void error(int c)
-{
-    if (c == 1)
-    {
-        cout << "build error";
-    }
-}
-
+// function with time O(n) for any m
 int jf(int n, int m)
 {
     if (n == 1)
@@ -23,12 +13,9 @@ int jf(int n, int m)
     {
         return (1 + (jf(n - 1, m) + m - 1) % n);
     }
-    /*else
-    {
-        error(1);
-    }*/
 }
 
+// function with time O(ln(n)) only for m = 2
 int jfln_bin(int n)
 {
     if (n == 1)
@@ -45,6 +32,7 @@ int jfln_bin(int n)
     }
 }
 
+// function with time O(1) only for m = 2
 int jf0_bin(int n)
 {
     return (2 * (n - pow(2, (int)(log(n)/log(2)))) + 1);
@@ -52,19 +40,7 @@ int jf0_bin(int n)
 
 int main()
 {
-    cout << jf(41, 2) << endl;
+    cout << jf(41, 2) << endl; 
     cout << jfln_bin(41) << endl;
     cout << jf0_bin(41);
-    //std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
